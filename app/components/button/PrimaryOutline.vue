40e763652191e3cffc533820748b6110609bf7a8
Brian Kintz <btkintz@gmail.com>
2023-04-25 00:39:05 +0200

    Reduce button motion

--- app/components/button/PrimaryOutline.vue
+++ app/components/button/PrimaryOutline.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="rounded-xl bg-slate-50 dark:bg-slate-600 border-2 border-teal-500 py-1.5 px-3.5 font-medium text-teal-500 dark:text-teal-400 hover:bg-slate-100 dark:hover:bg-slate-700 hover:scale-[1.03] hover:shadow-sm transition-all focus:outline-none focus-visible:ring-2 focus-visible:ring-teal-500 focus-visible:ring-offset-2"
+    class="rounded-xl bg-slate-50 dark:bg-slate-600 border-2 border-teal-500 py-1.5 px-3.5 font-medium text-teal-500 dark:text-teal-400 hover:bg-slate-100 dark:hover:bg-slate-700 transition-colors focus:outline-none focus-visible:ring-2 focus-visible:ring-teal-500 focus-visible:ring-offset-2"
   >
     <slot />
   </button>
--- app/components/button/Secondary.vue
+++ app/components/button/Secondary.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="rounded-xl bg-indigo-500 py-2 px-4 text-white hover:bg-indigo-600 hover:scale-[1.03] hover:shadow-sm transition-all focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2"
+    class="rounded-xl bg-indigo-500 py-2 px-4 text-white hover:bg-indigo-600 transition-colors focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2"
   >
     <slot />
   </button>
